e11fc240505f5792f96dff4cf4ce94ecd40c5775
Maya Mathur <mmathur@stanford.edu>
2015-04-02 14:36:46 -0700

    GENSURV: Generates survival times and logs do print out, but can't find generated data

--- push_to_sherlock.R
+++ push_to_sherlock.R
@@ -2,15 +2,29 @@
 # note: will give lots of permissions errors, but it does work
 system( "scp -r ~/Dropbox/QSU/Mathur/MY_PAPERS/TVC/Code/git_repo/time-varying-coeffs/for_sherlock/* mmathur@sherlock:/share/PI/manishad/tvc" )
 
-# run them
+
+######### GENCOV ######### 
 # sbatch /share/PI/manishad/tvc/genCov/sbatch/1.sbatch -p manishad
-# sbatch /share/PI/manishad/tvc/genSurv/sbatch/0001_genSurv.sbatch -p manisha
 
 # look at genCov results
 # cd /share/PI/manishad/tvc/genCov/output/generated_data
 
 # look at genCov sbatches
 # cd /share/PI/manishad/tvc/genCov/sbatch
+
+
+######### GENSURV ######### 
+
+# sbatch /share/PI/manishad/tvc/genSurv/sbatch/0001_genSurv.sbatch -p manisha
+
+# look at genSurv sbatches
+# cd /share/PI/manishad/tvc/genSurv/sbatch
+
+# look at genSurv results
+# cd /share/PI/manishad/tvc/genSurv/output/generated_data
+
+# look at genSurv log
+# cd /share/PI/manishad/tvc/genSurv/output/log
 
 # copy completed dataset back to local machine
 system( "scp mmathur@sherlock:/share/PI/manishad/tvc/genSurv/datasets/* ~/Dropbox/QSU/Mathur/MY_PAPERS/TVC/Code/git_repo/time-varying-coeffs/test_data " )
